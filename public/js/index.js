b9c361747a9fb035dc4e7ac4c67b7d8c0a9c217e
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-30 20:38:07 -0500

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io(); //critical to listen for events
 
+function scrollToBottom () {
+    // Selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -16,7 +32,8 @@ socket.on('newMessage', function (message) {
         from: message.from,
         createdAt: formattedTime
     });
-    $('#message').append(html);
+    $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -27,7 +44,8 @@ socket.on('newLocationMessage', function (message) {
         from: message.from,
         createdAt: formattedTime
     });
-    $('#message').append(html);
+    $('#messages').append(html);
+    scrollToBottom();
 });
 
 $('#send').on('click', function(e) {
