359e0b5cfbb2aaf485df2de035b3f1570d213dc1
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-31 01:52:06 -0500

    Add Users class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -5,13 +5,14 @@ const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
-
+const {Users} = require('./utils/users');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server); // allows us to listen and emit based on events
+var users = new Users();
 
 app.use(express.static(publicPath));
 
@@ -20,10 +21,13 @@ io.on('connection', (socket) => { // socket represents the individual user conne
 
     socket.on('join', (params, callback) => {
         if (!isRealString(params.name) || !isRealString(params.room)){
-            callback('Name and room name are required.')
+            return callback('Name and room name are required.')
         }
 
         socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
         // socket.leave(params.room);
 
         // io.emit                  this emits to everyone connected
@@ -51,6 +55,14 @@ io.on('connection', (socket) => { // socket represents the individual user conne
 
     socket.on('disconnect', () => {
         console.log('User was disconnected');
+        var user = users.removeUser(socket.id);
+        
+        if (user) {
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`));
+            
+
+        }
     });
 }); // lets you register an event listener
 
